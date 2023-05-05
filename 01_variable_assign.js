6a54429510e31f4beeeb90819ebe6a3f45cd3a05
supriyayss <supriya.shinde0016@gmail.com>
2023-05-05 15:09:15 +0530

    created JS file for Variable assignment 1

---
+++ 01_variable_assign.js
@@ -0,0 +1,25 @@
+var myFullName;
+console.log(myFullName);
+myFullName = "Supriya Salunkhe";
+console.log(myFullName);
+
+myFullName = "Supriya Yuvraj Salunkhe";
+console.log(myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Supriya";
+console.log("First Name: " + firstName);
+var lastName = "Salunkhe";
+console.log("Last Name: " + lastName);
+var age = 26;
+console.log("Age: " + age);
+var address = "Phase 1, Hinjewadi, Pune";
+console.log("Address: " + address);
+var pincode = 411057;
+console.log("Pincode: " + pincode);
+var gender = "Female";
+console.log("Gender: " + gender);
+var isMarried = "Yes";
+console.log("Mariatal Status: " + isMarried);
