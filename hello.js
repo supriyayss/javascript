88efc5cb9e0283398d2b0e6908138087389a07e1
supriyayss <supriya.shinde0016@gmail.com>
2023-05-05 15:10:48 +0530

    learn declaration, Initialization, undefined and variable update

---
+++ hello.js
@@ -0,0 +1,18 @@
+
+
+console.log("Supriya Salunkhe...");
+console.log("Near Wipro Circle, Phase 1, Hinjewadi, Pune.");
+
+var firstName = "Supriya";
+console.log( firstName);
+
+var age; // variable declaration
+age = 22; // Initialization
+console.log(age);
+
+var holder;
+console.log(holder);// undefined
+holder = "My spec";
+console.log(holder);
+holder = "Sachin Spec";// variable update 
+console.log(holder);
