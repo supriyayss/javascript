8b941385604d3c55075b00b6d18ff61d07e92b66
supriyayss <supriya.shinde0016@gmail.com>
2023-05-07 00:07:19 +0530

    commit 02_variableBasicsAssig.js file

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,27 @@
+var age = 2.5;
+var weight = 12;
+var height = 2.96;
+
+console.log("Number Datatypes");
+console.log( "Age:", age, "       ", "Type: ", typeof age );
+console.log( "Weight:", weight, "     ", "Type: ", typeof weight );
+console.log( "Last Name:", height, "", "Type: ", typeof height );
+console.log("------");
+
+console.log("String Datatype");
+var firstName = "Yugandhara";
+var middleName = "Abhijeet";
+var lastName = "Shinde";
+
+console.log( "First Name:", firstName, " ", "Type: ", typeof firstName );
+console.log( "Middle Name:", middleName, " ", "Type: ", typeof middleName );
+console.log( "Last Name:", lastName, "     ", "Type: ", typeof lastName );
+console.log("------");
+
+console.log("Boolean Datatype")
+var isDancer = true;
+var isLikeFruits = false;
+var isActive = true;
+console.log("She is Dancer:", isDancer, "     ", "Type", typeof isDancer);
+console.log("She like Fruits:", isLikeFruits, " ", "Type", typeof isLikeFruits);
+console.log("She is Active:", isActive, "     ", "Type", typeof isActive);
