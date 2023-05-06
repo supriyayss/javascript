92eb80138d5e160b63e18721b039c6c1fcbb2e75
supriyayss <supriya.shinde0016@gmail.com>
2023-05-07 00:08:27 +0530

    commit 03_typeOf.js file

---
+++ 03_typeOf.js
@@ -0,0 +1,17 @@
+var bankName;
+console.log("Bank Name:", bankName, " ", "Type:", typeof bankName);
+bankName = "SBI Bank";
+console.log("Bank Name:", bankName, "  ", "Type:", typeof bankName);
+
+var marks = '90%';
+console.log("Marks:", marks, "           ", "Type:", typeof marks);
+
+var isWorking;
+console.log("Is Working:", isWorking, "   ", "Type:", typeof isWorking);
+isWorking = true;
+console.log("Is Working:", isWorking, "        ", "Type:", typeof isWorking);
+
+var totalCount = "Hundred and Seven";
+console.log("Total count:", totalCount, "  ", "Type:", typeof totalCount);
+totalCount = 107;
+console.log("Total count:", totalCount, "                ", "Type:", typeof totalCount);
