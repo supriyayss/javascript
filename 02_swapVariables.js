9a749eb1c71144539273e8fbbf066d6aec67fb56
supriyayss <supriya.shinde0016@gmail.com>
2023-05-11 14:49:16 +0530

    Swapping Variables assignment completed

---
+++ 02_swapVariables.js
@@ -0,0 +1,26 @@
+//Swap two variables
+console.log("---Swapping Two Variables---");
+var sweety = "Sweety";
+var cutie = "Cutie";
+var temp;
+console.log("Before Swapping:--", "sweety:", sweety,"|","cutie:", cutie);
+
+temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("After Swapping:--", "sweety:", sweety,"|","cutie:", cutie);
+console.log(" ");
+
+//Swap three variables
+console.log("---Swapping Three Variables---");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+var temp;
+console.log("Before Swapping:--", "num1:", num1,"|","num2:", num2,"|","num3:", num3);
+
+temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("After Swapping:--", "num1:", num1,"|","num2:", num2,"|","num3:", num3);
