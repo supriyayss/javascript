6fff62bc6886d264a066bf4c660b5918600f08ad
supriyayss <supriya.shinde0016@gmail.com>
2023-05-12 13:44:22 +0530

    String assignment_01 completed
    we used length property in function with no argument no return value

---
+++ stringAssig_01.js
@@ -0,0 +1,13 @@
+function stringBasics(){
+    console.log("My dream company is","Wipro");
+    var hobbyOne ="Learn New Technologies";
+    var hobbyTwo = "Drawing";
+    var hobbyThree = "Dancing";
+    console.log("My Hobbies are:",hobbyOne,",",hobbyTwo,",",hobbyThree);
+
+    var hobbyOneLength = hobbyOne.length;
+    var hobbyTwoLength = hobbyTwo.length;
+    var hobbyThreeLength = hobbyThree.length;
+    console.log("Total Number Of Character in all Hobbies:",hobbyOneLength + hobbyTwoLength +hobbyThreeLength);
+}
+stringBasics();
