7fce764217acd6faaaead7d12981c6295a5d673f
supriyayss <supriya.shinde0016@gmail.com>
2023-05-15 17:55:18 +0530

    Learn Functions
    1.function with argument no return value
    2.function with argument with return value

---
+++ 03_function.js
@@ -0,0 +1,41 @@
+function show() {
+    console.log("Show function");
+}
+console.log("Before function call");
+show(); // Function call or invoke
+console.log("After function");
+show();
+show();
+show();
+
+var numOne = 100;
+var numTwo = 200;
+
+var name1 = "Billgates";
+var name2 = "Elon Musk";
+
+var fruitApple = "Apple";
+var fruitMango = "Mango";
+
+function swapVariables(valueOne, valueTwo){ // Arguments: valueOne =100  valueTwo = 200 var valueThree;
+    console.log("Before Swap: ", valueOne, valueTwo);
+    var temp = valueOne;
+    valueOne = valueTwo; 
+    valueTwo = temp;
+    console.log("After Swap: ", valueOne, valueTwo);
+}
+swapVariables(numOne, numTwo); // 100 200
+swapVariables(name1, name2);
+swapVariables(fruitApple, fruitMango);
+
+
+
+// function with argument with return value
+function addition(num1, num2) {
+    var result  = num1 + num2;// result = 300
+    return result;
+}
+var returnValue = addition(100, 200);
+console.log("Addition of number is: ", returnValue);
+var returnResult = addition(890.400, 600.3456);
+console.log("Addition of number is: ", returnResult);
