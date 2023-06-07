9b5b39154628bc54045fdf4edc0c75adc66114bc
supriyayss <supriya.shinde0016@gmail.com>
2023-06-07 23:20:31 +0530

    modified

--- 03_functionExpression.js
+++ 03_functionExpression.js
@@ -1,6 +1,7 @@
-var multiplication = function (one, two, three) {
-    var result = one*two*three;
-    return `Multiplication is: ${result}`;
+let multiplication = function (one, two, three) {
+    return (one*two*three);
+    // return `Multiplication is: ${result}`;
 }
-var result = multiplication(2, 2, 2 );
-console.log(result);
+// var result = multiplication(2, 2, 2 );
+// console.log(result);
+console.log(multiplication(2,2,2));
